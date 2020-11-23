cbe5c652bffbd9e1792e82a206f1ec280b6601ec
tangkaiqiang <justforgit@163.com>
2020-11-23 15:45:06 +0800

    add test

--- test/spec.js
+++ test/spec.js
@@ -13,20 +13,28 @@ let page = null;
 describe('Application launch', function () {
   this.timeout(10000);
 
-  beforeEach(async function () {
+  before(async function () {
     app = await puppeteer.connect({ browserURL: 'http://localhost:9200' });
     [page] = await app.pages();
     page.setViewport({ width: 1080, height: 840 });
   });
 
-  afterEach(async function () {
+  after(async function () {
     if (app) {
       app.close();
     }
   });
 
   it('should have a button with class "add-tpl"', async () => {
-    const element = await page.$$('.add-tpl');
-    assert.strictEqual(element.length, 1);
+    const btns = await page.$$('.add-tpl');
+    assert.strictEqual(btns.length, 1);
+  });
+
+  it('should show form successfully', async () => {
+    const tplAddBtn = await page.$('.add-tpl');
+    await tplAddBtn.click();
+    const className = await page.$eval('.form', (el) => el.getAttribute('class'));
+    const isShow = className.indexOf('form-show') !== -1;
+    assert.strictEqual(isShow, true);
   });
 });
