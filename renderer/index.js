9b769b0590bc3c181b1e6e53787271e1d1351775
tangkaiqiang <justforgit@163.com>
2020-11-27 10:14:30 +0800

    添加测试用例

--- renderer/index.js
+++ renderer/index.js
@@ -2,7 +2,7 @@
  * @Description:渲染进程主入口
  * @Author: tkiddo
  * @Date: 2020-11-20 10:56:42
- * @LastEditTime: 2020-11-26 10:34:57
+ * @LastEditTime: 2020-11-27 09:43:14
  * @LastEditors: tkiddo
  */
 require('./common.js');
--- test/spec.js
+++ test/spec.js
@@ -3,12 +3,14 @@
  * @Author: tkiddo
  * @Date: 2020-11-26 15:20:27
  * @LastEditors: tkiddo
- * @LastEditTime: 2020-11-26 19:35:57
+ * @LastEditTime: 2020-11-27 10:13:35
  */
 const electron = require('electron');
 const puppeteer = require('puppeteer-core');
 const { spawn } = require('child_process');
 const assert = require('assert');
+const fs = require('fs');
+const path = require('path');
 
 let spawnProcess = spawn(electron, ['.', `--remote-debugging-port=9200`], {
   shell: true
@@ -17,10 +19,15 @@ let spawnProcess = spawn(electron, ['.', `--remote-debugging-port=9200`], {
 let app = null;
 let page = null;
 
+const emptyData = () => {
+  fs.writeFileSync(path.join(__dirname, '../share/tplList.json'), JSON.stringify([], null, 2));
+};
+
 describe('Application launch', function () {
   this.timeout(10000);
 
   before(async function () {
+    emptyData();
     app = await puppeteer.connect({ browserURL: 'http://localhost:9200' });
     [page] = await app.pages();
     page.setViewport({ width: 1080, height: 840 });
@@ -29,6 +36,7 @@ describe('Application launch', function () {
   after(async function () {
     if (app) {
       app.close();
+      emptyData();
     }
   });
 
@@ -38,8 +46,7 @@ describe('Application launch', function () {
   });
 
   it('第一次运行时，数据模版列表为空', async () => {
-    const tplList = await page.$('.tpl-list');
-    const len = tplList.children.length;
+    const len = await page.$eval('.tpl-list', (el) => el.children.length);
     assert.strictEqual(len, 0);
   });
 
@@ -49,5 +56,17 @@ describe('Application launch', function () {
     const className = await page.$eval('.form', (el) => el.getAttribute('class'));
     const isShow = className.indexOf('form-show') !== -1;
     assert.strictEqual(isShow, true);
+  });
+
+  it('添加数据模版操作后，列表中数据加一条', async () => {
+    await (await page.$('.add-tpl')).click();
+    await page.waitForSelector('#tpl-add-form', { visiable: true });
+    await page.type('input[name = "name"]', 'example');
+    const value = await page.$eval('input[name = "name"]', (el) => el.value);
+    assert.strictEqual(value, 'example');
+    await page.waitForSelector('#tpl-sure-btn');
+    await page.evaluate(() => document.querySelector('#tpl-sure-btn').click());
+    const len = await page.$eval('.tpl-list', (el) => el.children.length);
+    assert.strictEqual(len, 1);
   });
 });
