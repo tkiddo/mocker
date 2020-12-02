9ed18968cd36442801965a2a7c5a9706c42eb534
tangkaiqiang <justforgit@163.com>
2020-12-02 15:24:33 +0800

    打包选项

--- test/spec.js
+++ test/spec.js
@@ -3,7 +3,7 @@
  * @Author: tkiddo
  * @Date: 2020-11-26 15:20:27
  * @LastEditors: tkiddo
- * @LastEditTime: 2020-11-28 14:05:21
+ * @LastEditTime: 2020-12-02 15:22:52
  */
 const electron = require('electron');
 const puppeteer = require('puppeteer-core');
@@ -27,7 +27,7 @@ describe('Application launch', function () {
   this.timeout(10000);
 
   before(async function () {
-    // emptyData();
+    emptyData();
     app = await puppeteer.connect({ browserURL: 'http://localhost:9200' });
     [page] = await app.pages();
     page.setViewport({ width: 1080, height: 840 });
@@ -36,7 +36,7 @@ describe('Application launch', function () {
   after(async function () {
     if (app) {
       app.close();
-      // emptyData();
+      emptyData();
     }
   });
 
@@ -47,7 +47,7 @@ describe('Application launch', function () {
 
   it('第一次运行时，数据模版列表为空', async () => {
     const len = await page.$eval('.tpl-list', (el) => el.children.length);
-    assert.strictEqual(len, 0);
+    assert.strictEqual(len, 1);
   });
 
   it('点击添加数据模版按钮，表单弹出', async () => {
@@ -61,20 +61,20 @@ describe('Application launch', function () {
   it('添加数据模版操作后，列表中数据加一条,并且是激活状态', async () => {
     await (await page.$('.add-tpl')).click();
     await page.waitForSelector('#tpl-add-form', { visiable: true });
-    await page.type('input[name = "name"]', 'example');
     const value = await page.$eval('input[name = "name"]', (el) => el.value);
-    assert.strictEqual(value, 'example');
+    assert.strictEqual(value, 'students');
     await page.waitForSelector('#tpl-sure-btn');
     await page.evaluate(() => document.querySelector('#tpl-sure-btn').click());
     const len = await page.$eval('.tpl-list', (el) => el.children.length);
-    assert.strictEqual(len, 1);
-    const className = await page.$eval('.tpl-item:last-child', (el) => el.getAttribute('class'));
+    assert.strictEqual(len, 2);
+    const className = await page.$eval('.tpl-item:first-child', (el) => el.getAttribute('class'));
     assert.strictEqual(className.indexOf('tpl-item-active') !== -1, true);
   });
 
   it('点击垃圾桶图标，删除对应的数据模版', async () => {
+    await page.waitForSelector('#tpl-add-form', { visiable: true });
     await page.click('.icon-ashbin');
     const len = await page.$eval('.tpl-list', (el) => el.children.length);
-    assert.strictEqual(len, 0);
+    assert.strictEqual(len, 1);
   });
 });
