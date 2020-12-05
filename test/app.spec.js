6a526c5b7b2b549d7e41d8e11be27cbb6ece338f
tkiddo <justforgit@163.com>
2020-12-05 16:41:23 +0800

    测试更新

--- test/app.spec.js
+++ test/app.spec.js
@@ -1,56 +1,56 @@
-// /*
-//  * @Description: test
-//  * @Author: tkiddo
-//  * @Date: 2020-12-02 14:37:22
-//  * @LastEditTime: 2020-12-02 15:13:12
-//  * @LastEditors: tkiddo
-//  */
+/*
+ * @Description: test
+ * @Author: tkiddo
+ * @Date: 2020-12-02 14:37:22
+ * @LastEditTime: 2020-12-05 14:13:46
+ * @LastEditors: tkiddo
+ */
 
-// const Application = require('spectron').Application;
-// const assert = require('assert');
-// const electronPath = require('electron'); // Require Electron from the binaries included in node_modules.
-// const path = require('path');
+const Application = require('spectron').Application;
+const assert = require('assert');
+const electronPath = require('electron'); // Require Electron from the binaries included in node_modules.
+const path = require('path');
 
-// const app = new Application({
-//   // Your electron path can be any binary
-//   // i.e for OSX an example path could be '/Applications/MyApp.app/Contents/MacOS/MyApp'
-//   // But for the sake of the example we fetch it from our node_modules.
-//   path: electronPath,
+const app = new Application({
+  // Your electron path can be any binary
+  // i.e for OSX an example path could be '/Applications/MyApp.app/Contents/MacOS/MyApp'
+  // But for the sake of the example we fetch it from our node_modules.
+  path: electronPath,
 
-//   // Assuming you have the following directory structure
+  // Assuming you have the following directory structure
 
-//   //  |__ my project
-//   //     |__ ...
-//   //     |__ main.js
-//   //     |__ package.json
-//   //     |__ index.html
-//   //     |__ ...
-//   //     |__ test
-//   //        |__ spec.js  <- You are here! ~ Well you should be.
+  //  |__ my project
+  //     |__ ...
+  //     |__ main.js
+  //     |__ package.json
+  //     |__ index.html
+  //     |__ ...
+  //     |__ test
+  //        |__ spec.js  <- You are here! ~ Well you should be.
 
-//   // The following line tells spectron to look and use the main.js file
-//   // and the package.json located 1 level above.
-//   args: [path.join(__dirname, '..')]
-// });
+  // The following line tells spectron to look and use the main.js file
+  // and the package.json located 1 level above.
+  args: [path.join(__dirname, '..')]
+});
 
-// describe('Application launch', function () {
-//   this.timeout(10000);
+describe('Application launch', function () {
+  this.timeout(10000);
 
-//   beforeEach(function () {
-//     return app.start();
-//   });
+  beforeEach(function () {
+    return app.start();
+  });
 
-//   afterEach(function () {
-//     if (app && app.isRunning()) {
-//       return app.stop();
-//     }
-//   });
+  afterEach(function () {
+    if (app && app.isRunning()) {
+      return app.stop();
+    }
+  });
 
-//   it('shows an initial window', function () {
-//     return app.client.getWindowCount().then(function (count) {
-//       assert.strictEqual(count, 1);
-//       // Please note that getWindowCount() will return 2 if `dev tools` are opened.
-//       // assert.equal(count, 2)
-//     });
-//   });
-// });
+  it('shows an initial window', function () {
+    return app.client.getWindowCount().then(function (count) {
+      assert.strictEqual(count, 1);
+      // Please note that getWindowCount() will return 2 if `dev tools` are opened.
+      // assert.equal(count, 2)
+    });
+  });
+});
--- test/spec.js
+++ test/spec.js
@@ -1,80 +1,80 @@
-/*
- * @Description:
- * @Author: tkiddo
- * @Date: 2020-11-26 15:20:27
- * @LastEditors: tkiddo
- * @LastEditTime: 2020-12-03 13:48:48
- */
-const electron = require('electron');
-const puppeteer = require('puppeteer-core');
-const { spawn } = require('child_process');
-const assert = require('assert');
-const fs = require('fs');
-const { listFilePath } = require('../main/constants');
+// /*
+//  * @Description:
+//  * @Author: tkiddo
+//  * @Date: 2020-11-26 15:20:27
+//  * @LastEditors: tkiddo
+//  * @LastEditTime: 2020-12-03 13:48:48
+//  */
+// const electron = require('electron');
+// const puppeteer = require('puppeteer-core');
+// const { spawn } = require('child_process');
+// const assert = require('assert');
+// const fs = require('fs');
+// const { listFilePath } = require('../main/constants');
 
-let spawnProcess = spawn(electron, ['.', `--remote-debugging-port=9200`], {
-  shell: true
-});
+// let spawnProcess = spawn(electron, ['.', `--remote-debugging-port=9200`], {
+//   shell: true
+// });
 
-let app = null;
-let page = null;
+// let app = null;
+// let page = null;
 
-const emptyData = () => {
-  fs.writeFileSync(listFilePath, JSON.stringify([], null, 2));
-};
+// const emptyData = () => {
+//   fs.writeFileSync(listFilePath, JSON.stringify([], null, 2));
+// };
 
-describe('Application launch', function () {
-  this.timeout(10000);
+// describe('Application launch', function () {
+//   this.timeout(10000);
 
-  before(async function () {
-    emptyData();
-    app = await puppeteer.connect({ browserURL: 'http://localhost:9200' });
-    [page] = await app.pages();
-    page.setViewport({ width: 1080, height: 840 });
-  });
+//   before(async function () {
+//     emptyData();
+//     app = await puppeteer.connect({ browserURL: 'http://localhost:9200' });
+//     [page] = await app.pages();
+//     page.setViewport({ width: 1080, height: 840 });
+//   });
 
-  after(async function () {
-    if (app) {
-      app.close();
-      emptyData();
-    }
-  });
+//   after(async function () {
+//     if (app) {
+//       app.close();
+//       emptyData();
+//     }
+//   });
 
-  it('有一个添加数据模版按钮', async () => {
-    const btns = await page.$$('.add-tpl');
-    assert.strictEqual(btns.length, 1);
-  });
+//   it('有一个添加数据模版按钮', async () => {
+//     const btns = await page.$$('.add-tpl');
+//     assert.strictEqual(btns.length, 1);
+//   });
 
-  it('第一次运行时，数据模版列表为空', async () => {
-    const len = await page.$eval('.tpl-list', (el) => el.children.length);
-    assert.strictEqual(len, 1);
-  });
+//   it('第一次运行时，数据模版列表为空', async () => {
+//     const len = await page.$eval('.tpl-list', (el) => el.children.length);
+//     assert.strictEqual(len, 1);
+//   });
 
-  it('点击添加数据模版按钮，表单弹出', async () => {
-    const tplAddBtn = await page.$('.add-tpl');
-    await tplAddBtn.click();
-    const className = await page.$eval('.form', (el) => el.getAttribute('class'));
-    const isShow = className.indexOf('form-show') !== -1;
-    assert.strictEqual(isShow, true);
-  });
+//   it('点击添加数据模版按钮，表单弹出', async () => {
+//     const tplAddBtn = await page.$('.add-tpl');
+//     await tplAddBtn.click();
+//     const className = await page.$eval('.form', (el) => el.getAttribute('class'));
+//     const isShow = className.indexOf('form-show') !== -1;
+//     assert.strictEqual(isShow, true);
+//   });
 
-  it('添加数据模版操作后，列表中数据加一条,并且是激活状态', async () => {
-    await (await page.$('.add-tpl')).click();
-    await page.waitForSelector('#tpl-add-form', { visiable: true });
-    const value = await page.$eval('input[name = "name"]', (el) => el.value);
-    assert.strictEqual(value, 'students');
-    await page.waitForSelector('#tpl-sure-btn');
-    await page.evaluate(() => document.querySelector('#tpl-sure-btn').click());
-    const len = await page.$eval('.tpl-list', (el) => el.children.length);
-    assert.strictEqual(len, 2);
-    const className = await page.$eval('.tpl-item:first-child', (el) => el.getAttribute('class'));
-    assert.strictEqual(className.indexOf('tpl-item-active') !== -1, true);
-  });
+//   it('添加数据模版操作后，列表中数据加一条,并且是激活状态', async () => {
+//     await (await page.$('.add-tpl')).click();
+//     await page.waitForSelector('#tpl-add-form', { visiable: true });
+//     const value = await page.$eval('input[name = "name"]', (el) => el.value);
+//     assert.strictEqual(value, 'students');
+//     await page.waitForSelector('#tpl-sure-btn');
+//     await page.evaluate(() => document.querySelector('#tpl-sure-btn').click());
+//     const len = await page.$eval('.tpl-list', (el) => el.children.length);
+//     assert.strictEqual(len, 2);
+//     const className = await page.$eval('.tpl-item:first-child', (el) => el.getAttribute('class'));
+//     assert.strictEqual(className.indexOf('tpl-item-active') !== -1, true);
+//   });
 
-  it('点击垃圾桶图标，删除对应的数据模版', async () => {
-    await page.waitForSelector('#tpl-add-form', { visiable: true });
-    await page.click('.icon-ashbin');
-    const len = await page.$eval('.tpl-list', (el) => el.children.length);
-    assert.strictEqual(len, 1);
-  });
-});
+//   it('点击垃圾桶图标，删除对应的数据模版', async () => {
+//     await page.waitForSelector('#tpl-add-form', { visiable: true });
+//     await page.click('.icon-ashbin');
+//     const len = await page.$eval('.tpl-list', (el) => el.children.length);
+//     assert.strictEqual(len, 1);
+//   });
+// });
